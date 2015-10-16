bc9867279bf23f695b8978e033874fb5b956756f
Temitope Fowotade <fowotadetope@yahoo.com>
2015-10-16 23:08:36 +0100

    My First Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/* 
+ *iNote - A simple noteapp for capturing your thoughts on the go
+ *written by Temitope Fowotade
+ */
+
+(function () {
+  //create notes
+      //onclick save, a new note is created.
+  //edit notes
+      //onclick save, old note is updated.
+  //search notes by entering titles in search box.
+  //delete notes
+      //onclick trash, old note moves to the trash can.
+  //empty trash
+      //onclick empty trash, trash notes are deleted permanently.
+  //restore note
+      //onclick recover, trash note moves back to old notes.
+
+
+  //user authentication using local storage
+    //create account
+    //log in
+    //forgot password
+    //change password
+    //delete account
+    //log out
+
+  //onclick edit account, dropdown showing change password and delete account navigations.
+
+}())
