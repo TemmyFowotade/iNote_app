3ed1378277f7963a1cd3f99129883382a157b8ca
Temitope Fowotade <fowotadetope@yahoo.com>
2015-10-23 01:04:44 +0100

    working on design

--- js/app.js
+++ js/app.js
@@ -1,30 +1,70 @@
+//(function () {
 /* 
  *iNote - A simple noteapp for capturing your thoughts on the go
  *written by Temitope Fowotade
  */
 
-(function () {
-  //create notes
-      //onclick save, a new note is created.
-  //edit notes
+
+ var saveNote = document.getElementById("save_note");
+ var oldNoteArea = document.getElementById("old_note_area");
+ 
+ 
+ 
+	function createNote () {
+	  console.log("note created");
+	  function createNoteItem () {
+		  var noteItem = document.createElement("li");
+		  var label = document.createElement("label");
+		  var edit = document.createElement("button");
+		  var del = document.createElement("button");
+		  
+		  //create noteItem (list item) with label, button.edit, button.trash
+		  noteItem.appendChild(label);
+		  noteItem.appendChild(edit);
+		  noteItem.appendChild(del);
+		  return noteItem;
+		   
+	  }
+	  
+	  //append noteItem = oldNoteArea's first child
+	  oldNoteArea.appendChild(noteItem);
+	}
+    
+	  
+	  
+
+	function editNote () {
+	  
+	}
       //onclick save, old note is updated.
-  //search notes by entering titles in search box.
-  //delete notes
+	  
+ 
+	function searchNotes () {
+	  
+	}
+
+	
+	function trashNotes () {
+	  
+	}
       //onclick trash, old note moves to the trash can.
-  //empty trash
+	
+
+	function emptyTrash () {
+	  
+	}
       //onclick empty trash, trash notes are deleted permanently.
-  //restore note
+	  
+	  
+
+    function restoreNote () {
+	  
+    }
       //onclick recover, trash note moves back to old notes.
 
 
-  //user authentication using local storage
-    //create account
-    //log in
-    //forgot password
-    //change password
-    //delete account
-    //log out
-
-  //onclick edit account, dropdown showing change password and delete account navigations.
-
-}())
+	  
+	saveNote.onclick = createNote;  
+	
+ 
+//}())
---
+++ js/user.js
@@ -0,0 +1,15 @@
+ 
+ (function () {
+	 
+ //user authentication using local storage
+    //create account
+    //log in
+    //forgot password
+    //change password
+    //delete account
+    //log out
+
+  /*onclick edit account, dropdown showing change password and 
+  delete account navigations.*/
+  
+}())
