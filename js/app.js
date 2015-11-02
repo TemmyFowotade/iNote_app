8e478a94151033bc63d8b1660aecb3befa9b63a1
Temitope Fowotade <fowotadetope@yahoo.com>
2015-11-02 23:30:24 +0100

    edits notes

--- js/app.js
+++ js/app.js
@@ -6,28 +6,28 @@
  //(function () {
 	 
 	 
- function clean(node) {
-	  for(var n = 0; n < node.childNodes.length; n++) {
-		var child = node.childNodes[n];
-		if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
-			node.removeChild(child);
-			n--;
+	function clean(node) {
+		for(var n = 0; n < node.childNodes.length; n++) {
+			var child = node.childNodes[n];
+			if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
+				node.removeChild(child);
+				n--;
+			}
+			else if (child.nodeType === 1) {
+			  clean(child);
+			}
 		}
-		else if (child.nodeType === 1) {
-		  clean(child);
-		}
-	  }
 	}
 	clean(document.body);
  
- var titleNote = document.getElementById("title_area");
- var saveNote = document.getElementById("save_note");
- var oldNoteArea = document.getElementById("old_note_area");
- var ulElement = oldNoteArea.firstChild; 
- var editBtns = ulElement.getElementsByClassName("edit_note"); 
- var contentNote = document.getElementById("content_area");
- var title;
- var content;
+	 var titleNote = document.getElementById("title_area");
+	 var saveNote = document.getElementById("save_note");
+	 var oldNoteArea = document.getElementById("old_note_area");
+	 var ulElement = oldNoteArea.firstChild; 
+	 var editBtns = ulElement.getElementsByClassName("edit_note"); 
+	 var contentNote = document.getElementById("content_area");
+	 var title;
+	 var content;
 
  
  
@@ -39,6 +39,12 @@
 			newLi.setAttribute("class", "well well-sm");
 			
 			var newTitle = document.createTextNode(title);
+			
+			var newContent = document.createElement("input");
+			newContent.setAttribute("type", "text");
+			var newContentValue = document.createTextNode(content);
+			newContent.appendChild(newContentValue);
+			newContent.style.display = "none";
 			
 			var editBtn = document.createElement("input");
 			editBtn.setAttribute("type", "submit");
@@ -53,25 +59,34 @@
 			delBtn.onclick = deleteNote;
 		
 			newLi.appendChild(newTitle);
+			newLi.appendChild(newContent);
 			newLi.appendChild(editBtn);
 			newLi.appendChild(delBtn);
 			
 			ulElement.insertBefore(newLi, ulElement.firstChild);
 	}
+	
+	//validation for create note, 
+		//if title and content are empty, return false for onclick save_note
+		//once note is saved, clear contents of title and content areas.
 
-	  
-	  
+	
+
 
 	function editNote () {
-		console.log("edited!");
-		//onclick edit, 
-		//addEventListener 
-		//set this.title = titleNote.value
-		//set this.content = contentNote.value
-		//replaceChild li of parentNode ul
+		var titleEl = this.previousSibling.previousSibling.nodeValue;
+		titleNote.value = titleEl;
 		
+		var contentEl = this.previousSibling;
+		var contentNode = contentEl.childNodes[0].nodeValue;
+		contentNote.value = contentNode;
+		
+		var liElement = this.parentNode;
+		ulElement.removeChild(liElement);
 	}
-		
+	
+	//validation for edit note, after clicking edit, if user does not save note...undo edit action
+
 	
 	function deleteNote () {
 		console.log("deleted!");
