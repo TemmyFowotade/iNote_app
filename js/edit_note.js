6e47059a117ce8949fe50656e0051109853d52a2
Tosin Adesanya <oluwatosin@Oluwatosins-MacBook-Pro.local>
2016-01-28 16:19:39 +0100

    Trick git to believe

--- js/edit_note.js
+++ js/edit_note.js
@@ -1,14 +1,14 @@
 function clean (node) {
-	for(var n = 0; n < node.childNodes.length; n++) {
-		var child = node.childNodes[n];
-		if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
-			node.removeChild(child);
-			n--;
-		}
-		else if (child.nodeType === 1) {
-		  clean(child);
-		}
-	}
+  for(var n = 0; n < node.childNodes.length; n++) {
+    var child = node.childNodes[n];
+    if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
+      node.removeChild(child);
+      n--;
+    }
+    else if (child.nodeType === 1) {
+      clean(child);
+    }
+  }
 }
 
 clean(document.body);
@@ -27,58 +27,58 @@ var welcomeUser = document.getElementById("welcome_username");
 
 
 if( auth !== null ) {
-	welcomeUsers();
+  welcomeUsers();
 }
 
 function welcomeUsers() {
-	var myUser = usersRef.child(auth.uid);
-	myUser.once("value", function(snapshot) {
-		var username = snapshot.val().username;
-		welcomeUser.innerHTML = "Hello, " + username;
-	});
+  var myUser = usersRef.child(auth.uid);
+  myUser.once("value", function(snapshot) {
+    var username = snapshot.val().username;
+    welcomeUser.innerHTML = "Hello, " + username;
+  });
 }
 
 
 function editNote () {
-	var noteTitle = localStorage.getItem("title");
-	var noteContent = localStorage.getItem("content");
+  var noteTitle = localStorage.getItem("title");
+  var noteContent = localStorage.getItem("content");
 
-	titleNote.value = noteTitle;
-	contentNote.value = noteContent;
+  titleNote.value = noteTitle;
+  contentNote.value = noteContent;
 }
 editNote();
 
 
 
 function updateNote() {
-	if (titleNote.value !== "" && contentNote.value !== "") {
-		var title = titleNote.value;
-		var content = contentNote.value;
-		var liId = JSON.parse(localStorage.getItem("notekey"));
+  if (titleNote.value !== "" && contentNote.value !== "") {
+    var title = titleNote.value;
+    var content = contentNote.value;
+    var liId = JSON.parse(localStorage.getItem("notekey"));
 
 
-		var myUser = usersRef.child(auth.uid);
-		var notesRef = myUser.child("notes");
-		var notepath = notesRef.child(liId);
-		notepath.set({
-				title: title,
-				content: content,
-				noteid: liId
-				}, function() {
+    var myUser = usersRef.child(auth.uid);
+    var notesRef = myUser.child("notes");
+    var notepath = notesRef.child(liId);
+    notepath.set({
+        title: title,
+        content: content,
+        noteid: liId
+        }, function() {
          console.log("Note updated successfully: ", auth.uid);
         }
     );
 
     titleNote.value = "";
-		contentNote.value = "";
+    contentNote.value = "";
 
-		localStorage.removeItem("title");
-		localStorage.removeItem("content");
-		localStorage.removeItem("notekey");
+    localStorage.removeItem("title");
+    localStorage.removeItem("content");
+    localStorage.removeItem("notekey");
 
-		alert("note updated");
-		location = "notepage.html";
-	}
+    alert("note updated");
+    location = "notepage.html";
+  }
 }
 
 updateBtn.onclick = updateNote;
